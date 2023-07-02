c0923918aa4056dd104df6e3d41b5ea8c5aa40df
hatoo <hato2000@gmail.com>
2023-07-02 17:40:25 +0900

    basic_blocks

--- mybril/src/main.rs
+++ mybril/src/main.rs
@@ -1,6 +1,9 @@
-use std::io::{stdin, Read};
+use std::{
+    collections::HashMap,
+    io::{stdin, Read},
+};
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[derive(Deserialize, Debug)]
@@ -15,12 +18,16 @@ struct Function<'a> {
     name: &'a str,
 }
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 struct Instruction<'a> {
     op: &'a str,
+    #[serde(skip_serializing_if = "Option::is_none")]
     r#type: Option<&'a str>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     dest: Option<&'a str>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     value: Option<Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     args: Option<Vec<&'a str>>,
 }
 
@@ -40,8 +47,52 @@ fn main() {
     }
 }
 
+struct Cfg<'a> {
+    entry_label: String,
+    blocks: HashMap<String, Vec<Instruction<'a>>>,
+    predecessors: HashMap<String, Vec<String>>,
+    successors: HashMap<String, Vec<String>>,
+}
+
+fn basic_blocks<'a>(instrs: &[Instruction<'a>]) -> Vec<Vec<Instruction<'a>>> {
+    let mut blocks = Vec::new();
+    let mut block = Vec::new();
+    for instr in instrs {
+        match instr.op {
+            "br" | "jmp" => {
+                block.push(instr.clone());
+                blocks.push(block);
+                block = Vec::new();
+            }
+            "label" => {
+                if !block.is_empty() {
+                    blocks.push(block);
+                }
+                block = Vec::new();
+                block.push(instr.clone());
+            }
+            _ => {
+                block.push(instr.clone());
+            }
+        }
+    }
+    if !block.is_empty() {
+        blocks.push(block);
+    }
+    blocks
+}
+
 #[test]
 fn test_parse() {
-    const Json: &str = include_str!("../assets/add.json");
-    let bril: Bril = serde_json::from_str(Json).unwrap();
+    const JSON: &str = include_str!("../assets/add.json");
+    let _bril: Bril = serde_json::from_str(JSON).unwrap();
+}
+
+#[test]
+fn test_basic_blocks() {
+    const JSON: &str = include_str!("../assets/add.json");
+    let bril: Bril = serde_json::from_str(JSON).unwrap();
+
+    let basic_blocks = basic_blocks(&bril.functions[0].instrs);
+    insta::assert_json_snapshot!(basic_blocks);
 }
