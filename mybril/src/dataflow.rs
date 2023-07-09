5943b47d9f51c174af6405574b41c5f186f2341d
hatoo <hato2000@gmail.com>
2023-07-09 17:59:02 +0900

    Some generalize

--- mybril/src/dataflow.rs
+++ mybril/src/dataflow.rs
@@ -1,6 +1,117 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{hash_map::Entry, HashMap, HashSet},
+    marker::PhantomData,
+};
 
-use crate::{add_label, add_terminatior, partition, Function};
+use crate::{add_label, add_terminatior, partition, Function, Instruction};
+
+trait Merger<S>
+where
+    S: 'static,
+{
+    fn merge<'a, I: Iterator<Item = &'a S>>(&self, iter: I) -> S;
+}
+
+trait Tranfer<S> {
+    fn transfer(&self, instrs: &[Instruction], in_vars: &S) -> S;
+}
+
+struct Forward<S, M, T> {
+    m: M,
+    t: T,
+    _s: PhantomData<S>,
+}
+
+impl<S, M, T> Forward<S, M, T>
+where
+    S: 'static + PartialEq,
+    M: Merger<S>,
+    T: Tranfer<S>,
+{
+    fn analyze(&self, func: &Function) -> HashMap<String, (S, S)> {
+        let blocks = {
+            let mut blocks = partition(&func.instrs);
+            add_label(&mut blocks);
+            add_terminatior(&mut blocks);
+            blocks
+        };
+
+        let labels: Vec<&str> = blocks
+            .iter()
+            .map(|block| block[0].label.as_deref().unwrap())
+            .collect();
+
+        let label_map = blocks
+            .iter()
+            .map(|block| (block[0].label.as_deref().unwrap(), block.as_slice()))
+            .collect::<HashMap<_, _>>();
+
+        let (predecessors, successors) = {
+            let mut predecessors = HashMap::new();
+            let mut successors = HashMap::new();
+
+            for b in &blocks {
+                let start = b[0].label.as_deref().unwrap();
+
+                if let Some(labels) = b.last().unwrap().labels.as_ref() {
+                    for dest in labels {
+                        let dest = dest.as_str();
+                        successors
+                            .entry(start)
+                            .or_insert_with(HashSet::new)
+                            .insert(dest);
+                        predecessors
+                            .entry(dest)
+                            .or_insert_with(HashSet::new)
+                            .insert(start);
+                    }
+                }
+            }
+            (predecessors, successors)
+        };
+
+        let mut result: HashMap<String, (S, S)> = HashMap::new();
+
+        let mut work_list = labels.clone();
+
+        while let Some(label) = work_list.pop() {
+            let in_vars: S = self.m.merge(
+                predecessors
+                    .get(label)
+                    .unwrap_or(&Default::default())
+                    .iter()
+                    .flat_map(|&p| result.get(p).into_iter().map(|(_, out_vars)| out_vars)),
+            );
+
+            let out_vars = self.t.transfer(
+                label_map.get(label).unwrap_or(&Default::default()),
+                &in_vars,
+            );
+
+            match result.entry(label.to_string()) {
+                Entry::Occupied(mut io) => {
+                    let entry = io.get_mut();
+                    entry.0 = in_vars;
+                    if entry.1 != out_vars {
+                        entry.1 = out_vars;
+                        work_list.extend(
+                            successors
+                                .get(label)
+                                .into_iter()
+                                .flat_map(HashSet::iter)
+                                .cloned(),
+                        );
+                    }
+                }
+                Entry::Vacant(io) => {
+                    io.insert((in_vars, out_vars));
+                }
+            }
+        }
+
+        result
+    }
+}
 
 fn defined(func: &Function) -> Vec<(String, (HashSet<String>, HashSet<String>))> {
     let blocks = {
